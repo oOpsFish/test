ef0dfcbfa2e4ab37f47b329fd840ae6b9e599103
FisXan <1319416645@qq.com>
2023-10-28 19:45:55 +0800

    Supplementary notes.

--- Demo.java
+++ Demo.java
@@ -7,21 +7,21 @@ public class Demo {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		String str = null;
 		try{
-			str = bf.readLine();
+			str = bf.readLine();//用户输入一个字符串
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		char[] charArray = str.toCharArray();
 		for(int i=0;i<charArray.length;i++)
-			charArray[i]+=1+i+3;
+			charArray[i]+=1+i+3;//将每个字符的ASCII码加上它在字符串中的位置(1开始)和偏移值3
 		char temp =charArray[0];
 		charArray[0] = charArray[charArray.length-1];
-		charArray[charArray.length-1] = temp;
+		charArray[charArray.length-1] = temp;//将字符串的第一位和最后一位调换顺序
 		for (int i = 0; i < charArray.length / 2; i++) {
 		      temp = charArray[i];
 		      charArray[i] = charArray[charArray.length - 1 -i];
 		      charArray[charArray.length - 1 - i] = temp;
-		    }
+		    }//将字符串反转
 		System.out.println("加密后的结果是："+String.valueOf(charArray));
 	}
 	public static void cryption() {
@@ -30,26 +30,26 @@ public class Demo {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		String str = null;
 		try{
-			str = bf.readLine();
+			str = bf.readLine();//用户输入一个字符串
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		char[] charArray = str.toCharArray(); 
 		char temp =charArray[0];
 		charArray[0] = charArray[charArray.length-1];
-		charArray[charArray.length-1] = temp;
+		charArray[charArray.length-1] = temp;//将字符串的第一位和最后一位调换顺序
 		for (int i = 0; i < charArray.length / 2; i++) {
 		      temp = charArray[i];
 		      charArray[i] = charArray[charArray.length - 1 -i];
 		      charArray[charArray.length - 1 - i] = temp;
-		    }
+		    }//将字符串反转
 		for(int i=0;i<charArray.length;i++)
-			charArray[i]-=1+i+3;
+			charArray[i]-=1+i+3;//将每个字符的ASCII码加减去在字符串中的位置(1开始)和偏移值3
 		System.out.println("解密后的结果是："+String.valueOf(charArray));
 	}
 	public static void main(String arc[]) {
 		int choice = 0;
-		do {
+		do {//功能菜单
 		System.out.println("================================");
 		System.out.println("\t欢迎使用密码管理系统");
 		System.out.println("================================");
